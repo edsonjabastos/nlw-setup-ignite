e5e6f1215fb3574e0ce9abd158aab755a831c09d
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-01-17 21:30:38 -0300

    adding prisma

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ app.get("/", () => {
   return "Hello World";
 });
 
-app.listen({
-  port: 3333,
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running! 🚀");
+  });
